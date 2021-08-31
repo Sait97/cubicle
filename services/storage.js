3c9e9b11d141cf8754e19e880f2fef7315ac403f
Sait97 <44225063+Sait97@users.noreply.github.com>
2021-08-31 13:59:50 +0300

    Convert all data

--- services/storage.js
+++ services/storage.js
@@ -1,8 +1,5 @@
 const Cube = require('../models/Cube')
 
-const fs = require('fs').promises;
-const uniqid = require('uniqid');
-let data = {};
  /*
      {
         "name": "str",
@@ -12,14 +9,7 @@ let data = {};
     }
 */
 async function init(){
-    try{
-
-        data = JSON.parse(await fs.readFile('./models/data.json'));
-    }catch(err){
-        console.error('Error reading database');
-    }
-
-    return (req, res, next) => {
+  return (req, res, next) => {
         req.storage = {
             getAll,
             getById,
@@ -31,30 +21,31 @@ async function init(){
 }
 
 async function getAll(query){
-    let cubes = Object
-    .entries(data)
-    .map(([id, v]) =>  Object.assign({}, {id }, v));
-
+    const options = {}
+    
     if(query.search){
-        cubes = cubes.filter(c => c.name.toLowerCase().includes(query.search.toLowerCase()));
+        options.name = {$regex: query.search, $options: 'i'};
     }
     if(query.from){
-        cubes = cubes.filter(c => c.difficulty >= Number(query.from));
+        options.difficulty = {$gte: Number(query.from) };
     }
     if(query.to){
-        cubes = cubes.filter(c => c.difficulty <= Number(query.to));
+        options.difficulty = options.difficulty || {}
+        options.difficulty.$lte = Number(query.to)
+     };
+        
+        const cubes = Cube.find(options).lean();
+        return cubes
     }
 
-    return cubes
-}
 
 
 async function getById(id){
-    const cube = data[id];
+    const cube = await Cube.findById(id).lean();
     if(cube){
-        return Object.assign({}, { id}, cube);
+        return  cube;
     }else {
-        return undefined
+        return undefined;
     }
 }
 
@@ -65,22 +56,16 @@ async function create(cube){
 }
 
 async function edit(id, cube){
-    if(!data[id]){
+    const existing = await Cube.findById(id)
+
+    if(!existing){
         throw new ReferenceError('NO such ID in database')
     }
-    data[id] = cube
 
-    await persist();
+    Object.assign(existing, cube);
+    return existing.save()
 }
 
-async function persist(){
-    try{
-        await fs.writeFile('./models/data.json', JSON.stringify(data, null, 2));
- 
-     }catch(err){
-         console.error('Error writing database');
-     }
-}
 
 module.exports = {
     init,
